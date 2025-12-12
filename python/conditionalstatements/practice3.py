28dec7ded9500396bd69c98e5447b8a50bab456a
VASU CHAND <vasuchand9@gmail.com>
2025-12-12 22:45:20 +0530

    added practice3.py

---
+++ python/conditionalstatements/practice3.py
@@ -0,0 +1,7 @@
+#check if a number is multiple of 7 or not
+
+num= int(input('enter the number :'))
+if(num%7==0):
+    print("numer is multiple of 7")
+else:
+    print("number is not a multiple of 7")
