2a8f04e58e52d937fbe6e058b14a004e3517e52b
VASU CHAND <vasuchand9@gmail.com>
2025-12-15 20:39:12 +0530

    add list2.py file

---
+++ python/listandtuple/list2.py
@@ -0,0 +1,20 @@
+marks = [34 ,10 , 97 ,100 ]
+print( "marks of vaibhav are" ,   marks[2])
+marks.append(88)#adds a element
+marks.sort()# sorts in assending 
+print(marks)
+marks.sort(reverse=True)#sorts in descending
+print(marks)
+marks.reverse()#reverses the list
+print(marks)
+marks.insert( 34,5)
+#34 ki jagah ke 5 ajyga phir 5 ke
+#  aghe 34 aur bake saare same 34 
+#ke samne wale
+print(marks)
+marks.remove(34) # removes first occurence of ekement
+print(marks)
+marks.pop(2)
+print(marks)
+
+
