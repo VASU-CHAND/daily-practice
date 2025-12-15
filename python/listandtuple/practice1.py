c6526cf94d6d97a0eaec69cb25f759324ee0a43b
VASU CHAND <vasuchand9@gmail.com>
2025-12-15 21:28:45 +0530

    adding practice1.py

---
+++ python/listandtuple/practice1.py
@@ -0,0 +1,8 @@
+movies = []
+movie1= input("enter your first movie:")
+movie2= input("enter your second movie:")
+movie3= input("enter your third movie:")
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+print(movies)
